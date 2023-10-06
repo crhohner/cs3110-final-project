4a2189fc38795f7d92b8f52cc50527b1a541164e
Karen Shamir <ks959@cornell.edu>
2023-10-06 01:27:59 -0400

    Added model.ml file
    - note: it has errors

---
+++ lib/model.ml
@@ -0,0 +1,24 @@
+module Build = struct
+  type 'a list
+
+
+  (** adding tile at certain location on board, tile to add, row to add it to, locATION to add it to in list??*)
+  let add = 
+
+    
+  (**first tuple is row, index of og loc, second is row, index of new*)
+  let move = 
+
+
+  (* adds a tile to a new row *)
+  let new_row = 
+
+
+  (** checking if board is valid *)
+  let check lst = 
+
+
+  (*check if valid board after first turn, rows add up to at least 30*)
+  let check_first lst = 
+
+end
