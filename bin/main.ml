ac9386cd3b045c6fe3ed98e8101a9487f685d132
Caroline Hohner <crh255@cornell.edu>
2023-10-10 22:45:09 -0400

    Implement first player selection
    - Added caml ASCII art

--- bin/main.ml
+++ bin/main.ml
@@ -1,6 +1,8 @@
 open Rummikaml
 open Model
+open Random
 
+(*input processing / repeated stuff goes here *)
 let rec get_int_input () : int =
   try read_int ()
   with Failure s ->
@@ -18,10 +20,44 @@ let rec get_player_names n : string list =
       | "" -> "player" ^ string_of_int n
       | name -> name
     in
+    let _ = print_newline () in
     name :: get_player_names (n - 1)
 
-let _ = print_endline "welcome to rummikaml!"
+let rec set_first_player (names : string list) (name : string) : string list =
+  match names with
+  | h :: t -> if h = name then names else set_first_player (t @ [ h ]) name
+  | [] -> failwith ("player " ^ name ^ "not found")
+
+let choose_first_player (names : string list) =
+  let _ = Random.self_init () in
+  List.nth names (Random.int List.(length names))
+
+(*game starts here*)
+
+let _ = print_newline ()
+
+let _ =
+  print_endline
+    " [R]      _\n\
+    \ [U]  .--' |\n\
+    \ [M] /___^ |     .--.\n\
+    \ [M]     ) |    /     \\\n\
+    \ [I]    /  |  /`       '.\n\
+    \ [K]   |   '-'    /      \\\n\
+    \ [A]    \\         |       |\\\n\
+    \ [M]     \\    /   \\      /\\|\n\
+    \ [L]       \\  /'----`\\   /\n\
+    \           |||        \\ |\n\
+    \           ((|        ((|\n\
+    \           |||        |||\n\
+    \          //_(       //_("
+
+let _ = print_endline "welcome to rummikaml! \n"
 let _ = print_endline "let's get going. how many players?"
+let _ = print_newline ()
 let player_count = get_int_input ()
 let player_names = get_player_names player_count
+let first = choose_first_player player_names
+let player_names = set_first_player player_names first
 let game = Game.make player_names
+let _ = print_endline (first ^ " goes first! let's play!")
