2fa8d4060a9d1eb39835411e1c140248680c4bd8
Caroline Hohner <crh255@cornell.edu>
2023-10-10 22:50:53 -0400

    Document helper methods in CLI (bin/main.ml)

--- bin/main.ml
+++ bin/main.ml
@@ -3,12 +3,17 @@ open Model
 open Random
 
 (*input processing / repeated stuff goes here *)
+
+(** Returns the first valid integer input from the command line. Will keep
+    asking for a valid integer until one is given. *)
 let rec get_int_input () : int =
   try read_int ()
   with Failure s ->
     let _ = print_endline "please enter a valid integer" in
     get_int_input ()
 
+(** Returns a list of [n] player names given from the command line. Will
+    substitute empty names with "player[n]" for the [n]th player. *)
 let rec get_player_names n : string list =
   if n < 1 then []
   else
@@ -23,11 +28,15 @@ let rec get_player_names n : string list =
     let _ = print_newline () in
     name :: get_player_names (n - 1)
 
+(** Moves the head of a list of strings [names] to match an input string [name].
+    Raises Failure player [name] not found if [name] is not in [names].*)
 let rec set_first_player (names : string list) (name : string) : string list =
   match names with
   | h :: t -> if h = name then names else set_first_player (t @ [ h ]) name
-  | [] -> failwith ("player " ^ name ^ "not found")
+  | [] -> raise (Failure ("player " ^ name ^ "not found"))
 
+(** Returns a random string from a list of strings [names]. Used to randomly
+    select which player will go first. *)
 let choose_first_player (names : string list) =
   let _ = Random.self_init () in
   List.nth names (Random.int List.(length names))
