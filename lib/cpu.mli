f3827cb3d6c75ff4adbc8d5dd5055af907b020b1
Karen Shamir <ks959@cornell.edu>
2023-11-04 13:21:10 -0400

    cpu.mli added

---
+++ lib/cpu.mli
@@ -0,0 +1,33 @@
+open Model 
+
+val sort_by_num : tile list -> tile list 
+(** Returns cpu's hand as a list of tiles sorted first by number
+    then color (KRBY)**)
+
+val sort_by_color : tile list -> tile list 
+(** Returns cpu's hand as a list of tiles sorted first by color
+    then number**)
+
+val check_threes : tile list -> tile list option 
+(** Checks cpu's hand for possible valid set of 3 tiles. Returns 
+    [None] if no valid set exists and [Some v] otherwise**)
+
+val check_pairs : tile list -> tile list list 
+(** Checks cpu's hand for possible valid set of 2 tiles. Returns
+    list of all valid pairs of tiles. Makes sure there are no 
+    duplicate pairs**)
+
+val place_three : tile list list -> tile list -> tile list list 
+(** Takes board and valid set of 3 tiles in cpu's hand - places 
+    set down in board on new row and returns modified board**)
+
+val place_pair : tile list list -> tile list -> tile list list 
+(** Takes board and valid pair of tiles in cpu's hand - places 
+    pair down if possible and returns modified board**)
+
+val place_one : tile list list -> tile -> tile list list
+(** Takes board and tile in cpu's hand - places tile down if 
+    possible and returns modified board**)
+
+val turn : player -> tile list list -> player * tile list list 
+(** Completes a cpu turn**)
